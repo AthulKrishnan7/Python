b54fea395c3f6e4558135582fc8beb098adced3a
AthulKrishnan7 <krishnanathul138@gmail.com>
2024-11-05 19:02:08 +0530

    Python list

--- tEST1.py
+++ tEST1.py
@@ -1,12 +1,15 @@
 import math
 #num = int(input("Enter the number :"))
-new_list = [1,2,3,2,3,2,4,3,5,2,5]
-lis = []
+new_list = [1,2,3,2,3,2,4,3,5,2,5,7]
+even_lis = []
+odd_lis = []
 for x in new_list:
-    if x not in lis:
-        lis.append(x)
-        print(f"{x} is found  {new_list.count(x)} times")
-
+    if x%2 ==0:
+        even_lis.append(x)
+    else:
+        odd_lis.append(x)
+print(f' Sum of odd numbers of list {sum(odd_lis)}')
+print(f' Sum of odd numbers of list {sum(even_lis)}')
 
 
 
